2adc948a6b1614ea0bde5d65ded5422a4d795c46
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-04-06 22:24:36 -0500

    dynamic lists for adding expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,18 @@
-import React, { useState} from 'react';
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card.js';
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card.js";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React')
-
- 
-
-  const clickHandler = () => {
-    setTitle('Updated!')
-    console.log(title)
-  };
-
   return (
-    <Card className='expense-item'>
-        <ExpenseDate date = {props.date}/>
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
