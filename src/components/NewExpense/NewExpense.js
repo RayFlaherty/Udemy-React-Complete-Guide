14b16a4537c432010b2f73eaf4f3c1e2dc7204eb
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-04-14 21:27:32 -0500

    finishing expenses form and functionaliyt, setting up the chart function

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, SetIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    SetIsEditing(false);
+  };
+  const startEditingHandler = () => {
+    SetIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    SetIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData= {saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
     </div>
   );
 };
